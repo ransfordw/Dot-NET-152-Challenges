0156f043e4469816030d4094909728ac01722753
Ransford Walker <ransfordw@gmail.com>
2018-11-12 18:04:22 -0500

    finished challenge 6

---
+++ Challenge_6/CarRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_6
+{
+    public class CarRepository
+    {
+        private List<Car> _cars = new List<Car>();
+        private CarType _type;
+        
+        public List<Car> GetCarList()
+        {
+            return _cars;
+        }
+
+        public void AddCarToList(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public CarType GetFuelType(int fuelInput)
+        {
+            switch (fuelInput)
+            {
+                case 1:
+                    _type = CarType.Gas;
+                    break;
+                case 2:
+                    _type = CarType.Hybrid;
+                    break;
+                default:
+                    _type = CarType.Electric;
+                    break;
+            }
+            return _type;
+        }
+
+        public Car CreateCar(string make, string model, CarType type, int fuelage)
+        {
+            Car newCar = new Car()
+            {
+                CarMake = make,
+                CarModel = model,
+                FuelType = type,
+                MilesPer = fuelage
+            };
+            return newCar;
+        }
+
+        public bool YesNoResponse(string response)
+        {
+            response.ToLower();
+            if (response.Contains("y"))
+                return true;
+            else
+                return false;
+        }
+
+        public void RemoveCar(Car c)
+        {
+            _cars.Remove(c);
+        }
+    }
+}
---
+++ Challenge_6/ProgramUI.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Challenge_6
+{
+    public class ProgramUI
+    {
+        public void Run()
+        {
+            CarRepository carRepo = new CarRepository();
+            var cars = carRepo.GetCarList();
+            carRepo.AddCarToList(carRepo.CreateCar("Ford", "Explorer", CarType.Gas, 17));
+            carRepo.AddCarToList(carRepo.CreateCar("Tesla", "Model S", CarType.Electric, 54));
+            carRepo.AddCarToList(carRepo.CreateCar("Dodge", "Charger", CarType.Hybrid, 25));
+
+            Console.WriteLine("Welcome to Komodo!");
+            bool running = true;
+            while (running)
+            {
+                Console.WriteLine("What would you like to do?\n\t" +
+                    "1. See all cars\n\t" +
+                    "2. Add new car\n\t" +
+                    "3. Remove car\n\t" +
+                    "4. Update car\n\t" +
+                    "5. Exit");
+                int response = int.Parse(Console.ReadLine());
+                switch (response)
+                {
+                    case 1:
+                        PrintCars();
+                        break;
+                    case 2:
+                        Console.Write("Enter the make of the car: ");
+                        var make = Console.ReadLine();
+                        Console.Write("Enter the model of the car: ");
+                        var model = Console.ReadLine();
+                        Console.WriteLine("Select a fuel type: \n\t" +
+                            "1. Gas\n\t" +
+                            "2. Hybrid\n\t" +
+                            "3. Electric");
+                        var fuelInput = int.Parse(Console.ReadLine());
+                        var type = carRepo.GetFuelType(fuelInput);
+                        Console.Write("Enter the miles per fuel unit: ");
+                        var milesPer = int.Parse(Console.ReadLine());
+                        var car = carRepo.CreateCar(make, model, type, milesPer);
+                        carRepo.AddCarToList(car);
+                        break;
+                    case 3:
+                        Console.WriteLine("Would you like to remove a car?");
+                        var removeResp = Console.ReadLine();
+                        carRepo.YesNoResponse(removeResp);
+                        Console.WriteLine("Enter the number of the car you'd like to remove: ");
+
+                        var carRemove = PrintCars();
+                        var remove = cars[carRemove - 1];
+                        carRepo.RemoveCar(remove);
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter the number for the car you'd like to edit: ");
+                        var updateInput = PrintCars();
+                        var updateCar = cars[updateInput - 1];
+                        Console.WriteLine("Would you like to update the model? y/n");
+                        var resp = Console.ReadLine();
+                        if (carRepo.YesNoResponse(resp))
+                        {
+                            Console.WriteLine("Enter new model: ");
+                            updateCar.CarModel = Console.ReadLine();
+                        }
+                        Console.WriteLine("Would you like to update the make? y/n");
+                        resp = Console.ReadLine();
+                        if (carRepo.YesNoResponse(resp))
+                        {
+                            Console.WriteLine("Enter new make: ");
+                            updateCar.CarMake = Console.ReadLine();
+                        }
+                        Console.WriteLine("Would you like to update the fuel type? y/n");
+                        resp = Console.ReadLine();
+                        if (carRepo.YesNoResponse(resp))
+                        {
+                            Console.WriteLine("Select a fuel type: \n\t" +
+                           "1. Gas\n\t" +
+                           "2. Hybrid\n\t" +
+                           "3. Electric");
+                            var fueltypeUpdate = int.Parse(Console.ReadLine());
+                            updateCar.FuelType = carRepo.GetFuelType(fueltypeUpdate);
+                        }
+                        Console.WriteLine("Would you like to update the miles per fuel unit? y/n");
+                        resp = Console.ReadLine();
+                        if (carRepo.YesNoResponse(resp))
+                        {
+                            Console.WriteLine("Enter new miles per unit: ");
+                            updateCar.MilesPer = int.Parse(Console.ReadLine());
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            int PrintCars()
+            {
+                Console.WriteLine("Car Make \tModel \t Fuel Type \t Miles Per Fuel Unit");
+                int i = 1;
+                foreach (Car c in cars)
+                {
+                    Console.WriteLine($"{i}. {c.CarMake} \t{c.CarModel} \t{c.FuelType} \t{c.MilesPer}");
+                    i += 1;
+                }
+                var menuChoice = int.Parse(Console.ReadLine());
+                return menuChoice;
+            }
+        }
+    }
+}
